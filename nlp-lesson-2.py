b10f559383d785038761e0f6bff72003337925c8
Berke Bıçak <72358476+berkebicak@users.noreply.github.com>
2022-10-26 23:45:00 +0300

    Update nlp-lesson-2.py

--- nlp-lesson-2.py
+++ nlp-lesson-2.py
@@ -39,6 +39,7 @@ x = re.split("\s", txt.lower())
 print(x)
 print(x.count('the'))
 
+#########################################
 
 txt = "The Python regular expression is a sequence of characters, that defines a search Python "
 x = re.findall("Python$", txt)  # Python la bitiyor mu txt kontrol.
@@ -48,7 +49,8 @@ if x:
 else:
     print("no match")
 
-
+#########################################
+    
 txt = "The Python regular expression is a sequence of characters, that defines a search Python"
 x = re.findall("Python|C#", txt)
 print(x)
@@ -62,11 +64,11 @@ x2 = re.findall("thi+", txt.lower())  # + dan onceki harf (i) bir kere olsun
 print(x)
 print(x2)
 
-################################
+#########################################
+
 txt = "This is Python example"
 x = re.findall("[a-k]", txt)
 print(x)
-
 
 txt = "This is Python 100 example"
 x = re.findall("\d", txt)
@@ -76,7 +78,6 @@ txt = "This is Python example"
 x = re.findall("he..o", txt)
 print(x)
 
-
 txt = "This is Python ill"
 x = re.findall("il{2}", txt)
 print(x)
@@ -84,7 +85,6 @@ if x:
     print("matches")
 else:
     print("no match")
-
 
 txt = "This is Python example"
 x = re.findall("[^arn]", txt)
@@ -98,7 +98,6 @@ txt = "This is Python example 234"
 x = re.findall("[0123]", txt)
 print(x)
 
-
 txt = "This is Python example 28416 -1"
 x = re.findall("[0-9]", txt)
 print(x)
@@ -106,7 +105,8 @@ if x:
     print("matches")
 else:
     print("no match")
-
+    
+#########################################
 
 txt = "This is lesson starts at 11:45  71:45"
 # 0-5 aralığı ondalık kısımları kontrol eder 0-9 ise birinci basamakları 23:59
@@ -121,7 +121,6 @@ txt = "The Python lesson starts at python morning 11:45  71:45"
 x = re.findall("[Pp]ython", txt)
 print(x)
 
-
 txt = "The Python lesson all starts ill at python morning 11:45  71:45"
 x = re.findall("[ai]ll", txt)
 print(x)
@@ -132,13 +131,13 @@ x2 = re.sub("e", "E", txt)
 print(x)
 print(x2)
 
-
 p = re.compile("colou?r")  # u harfi ya 1 ya 0 tane olucak
 n = p.search("The color green")
 print(n.start())  # index at print
 n = p.search("abc")
 print(n)
 
+#########################################
 
 # PANDAS
 df = pd.read_csv('ds_salaries.csv')
